94a06aa79134da5aaa93c6eb6ed77f610a17dee9
francescacennamo <francesca.cennamo2@studenti.unina.it>
2026-06-22 11:59:57 +0200

    secondo commit

---
+++ src/main/java/Boundary/LoginForm.java
@@ -0,0 +1,11 @@
+package Boundary;
+
+import javax.swing.*;
+
+public class LoginForm {
+    private JPanel contentPane;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton passwordDimenticataButton;
+    private JButton accediButton;
+}
