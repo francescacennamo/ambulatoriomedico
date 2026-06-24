260a73e9f62ac65eabe93c9d0e10c01cbe8a029d
francescacennamo <francesca.cennamo2@studenti.unina.it>
2026-06-24 11:36:40 +0200

    primo commit

--- src/main/java/Database/SpecializzazioneRepository.java
+++ src/main/java/Database/SpecializzazioneRepository.java
@@ -5,13 +5,20 @@ import jakarta.persistence.EntityManager;
 import java.util.List;
 
 public class SpecializzazioneRepository {
+
     public List<Specializzazione> findAll() {
-        // Usiamo il riferimento esplicito a JpaUtil dello stesso package
-        EntityManager em = Database.JpaUtil.getInstance().getEntityManager();
+
+        EntityManager em = JpaUtil.getEntityManager();
+
         try {
-            return em.createQuery("SELECT s FROM Specializzazione s", Specializzazione.class)
+
+            return em.createQuery(
+                            "SELECT s FROM Specializzazione s",
+                            Specializzazione.class)
                     .getResultList();
+
         } finally {
+
             if (em != null) {
                 em.close();
             }
